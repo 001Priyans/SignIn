877f6bf810536dedbc846f82c3288f3b900de5d3
001Priyans <priyanshu.gupta22b@iiitg.ac.in>
2024-05-24 01:51:30 +0530

    signPage

---
+++ app.js
@@ -0,0 +1,25 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+let forget = document.getElementById("forget").style;
+
+signinBtn.onclick = function(){
+    nameField.style.maxHeight ="0";
+    title.innerHTML="Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+    forget.display="block";
+
+
+}
+signupBtn.onclick = function(){
+    nameField.style.maxHeight ="60px";
+    title.innerHTML="Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+    forget.display="none";
+
+    
+
+}
